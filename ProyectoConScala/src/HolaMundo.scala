0fbe136b81a331037ce16dd76620e4510c25fa2f
Alba Carmona Jamardo <acarmonaj@ayesa.com>
2019-05-29 11:54:50 +0200

    El mismo commit que el anterior pero en eclipse.

---
+++ ProyectoConScala/src/HolaMundo.scala
@@ -0,0 +1,14 @@
+
+
+class HolaMundo {
+  
+  object HolaMundo {
+  /* Este es mi primer programa Java usando Scala.
+   * Devuelve un Hola Mundo.
+   */
+    def main(args: Array[String]) {
+      println("Hola Mundo") // Devuelve Hola Mundo.
+    }
+  }
+  
+}
