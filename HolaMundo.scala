e4faf123f4a02b55e0c516b19aef4a48466db822
Alba Carmona Jamardo <acarmonaj@ayesa.com>
2019-05-29 11:41:16 +0200

    Clase de ejemplo.

---
+++ HolaMundo.scala
@@ -0,0 +1,9 @@
+object HolaMundo{
+	/* 
+	* Este es mi primer programa en Java para aprender Scala.
+	* Devuelve un Hola Mundo.	
+	*/
+	def main(args: Array[String]){
+		println("Hola Mundo"); // Aparece Hola Mundo.
+	}
+}
