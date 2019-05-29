b98287da6c283566e51b79c75f667aabed9be78e
Alba Carmona Jamardo <acarmonaj@ayesa.com>
2019-05-29 12:55:17 +0200

    Clase Localización.

--- Punto/src/Punto.scala
+++ Punto/src/Punto.scala
@@ -14,12 +14,29 @@ class Punto(val xc: Int, val yc: Int) {
   
 }
 
+class Localizacion(override val xc: Int, override val yc: Int, val zc: Int) extends Punto(xc, yc){
+    var z: Int = zc;
+    
+    def move(dx: Int, dy: Int, dz: Int){
+      x = x + dx;
+      y = y + dy;
+      z = z + dz;
+      
+      println("Localización punto X: " + x);
+      println("Localización punto Y: " + y);
+      println("Localización punto Z: " + z);
+    }
+  
+}
+
 object Demo{
   
   def main(args: Array[String]){
-    val pt = new Punto(10, 20);
+    //val pt = new Punto(10, 20);
+    //pt.move(10, 10); // Se mueve a la nueva localización.
     
-    pt.move(10, 10);
+    val loc = new Localizacion(10, 20, 15);
+    loc.move(10, 10, 5);
   }
   
 }
