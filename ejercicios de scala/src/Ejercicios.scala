0e65074e933168d8dbfa30c04c4e530c26b7488b
Alba Carmona Jamardo <acarmonaj@ayesa.com>
2019-06-13 13:38:51 +0200

    Juntar dos listas sin elementos repetidos mediante zip.

--- ejercicios de scala/src/Ejercicios.scala
+++ ejercicios de scala/src/Ejercicios.scala
@@ -5,6 +5,7 @@ class Ejercicios {
     
     /* Elimina los elementos duplicados de una lista de cadenas. */
     def quitaDuplicados(list: List[String]){
+
       val listaEntrada = ListBuffer(list)
       var copia : List[String] = List()
       
@@ -17,5 +18,24 @@ class Ejercicios {
       }
       
     }
+    
+    /* Junta dos listas intercalando los elementos sin repetir */
+    
+    def merge(lista1: List[Integer], lista2: List[Integer]): List[(Integer, Integer)] = {
+      
+      val listaRes = lista1 zip lista2
+      val copia : List[Integer] = List()
+      
+      for(x <- listaRes){
+        if(copia.contains(x)){
+          listaRes.updated(listaRes.indexOf(x), Nil)
+        }else{
+          copia ::: List(x)
+        }
+      }
+      
+      return listaRes
+    }
+    
   }
 }
